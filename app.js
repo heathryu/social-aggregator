1aaeb38b317a182e5f493d02da7d6679a3256f0a
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2019-03-07 18:49:15 +0000

    Add Passport Strategy for Google and routes

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ var session = require("express-session");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
 
 var app = express();
+
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, profile, done) {
+      done(null, error);
+    }
+  )
+);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -37,6 +53,7 @@ passport.deserializeUser(function(user, done) {
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var passport = require("passport");
+var router = express.Router();
+
+router.route("/google").get(
+  passport.authenticate("google", {
+    scope: ["https://www.googleapis.com/auth/gmail.readonly"]
+  })
+);
+
+router
+  .route("/google/callback")
+  .get(passport.authenticate("google", { failureRedirect: "/error" }), function(
+    req,
+    res
+  ) {
+    res.redirect("/users");
+  });
+
+module.exports = router;
